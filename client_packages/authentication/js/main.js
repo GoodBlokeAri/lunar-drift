3dfa386c774224484cd6f78697653504d7bc8d9c
Ari <oddwolfari@gmail.com>
2018-01-31 02:37:36 +1100

    Start of UI refactor
    Updated to the Boostrap Login/Register UI by Jim

--- client_packages/authentication/js/main.js
+++ client_packages/authentication/js/main.js
@@ -1,3 +1,21 @@
+/*
+	Element class is a helper to work easier with our UI elements (like auth form, some menus etc)
+
+	Showing element:
+		Without:
+			js: document.getElementById("auth").setAttribute("style", "display: block");
+			jquery: $("#auth").show();
+
+		With:
+			window.elements.auth.visible = true;
+
+*/
+
+/*
+
+onclick='sendData()'
+*/
+
 function sendData()
 {
     let loginName = document.getElementById("loginName"),
