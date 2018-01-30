2ef9eab5bc18cc9f548a52ec68e6950ec1ca1efc
Ari <oddwolfari@gmail.com>
2018-01-31 10:39:35 +1100

    removed unused pluto command

--- packages/LUNAR/commands/player.js
+++ packages/LUNAR/commands/player.js
@@ -2,19 +2,13 @@ mp.events.addCommand('help', (player, text) =>
 {
     if(player.admin < 1) return player.outputChatBox("<SERVER> You don't have access to this command!");
 
-    player.outputChatBox(">> (ADMIN HELP) /ping /team /pos");
+    player.outputChatBox(">> (ADMIN HELP) /ping /pos");
 });
 
 mp.events.addCommand('ping', (player) =>
 {
     let playerPing = player.ping
     player.notify('PONG! Ping: ' + playerPing);
-});
-
-mp.events.addCommand('team', (player) =>
-{
-    //could use flip bool here
-    player.notify('you swapped team!');
 });
 
 mp.events.addCommand('pos', (player) =>
