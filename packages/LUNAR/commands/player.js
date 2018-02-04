5f339e30d5fbefc7a4c791ddf04d0156727d80c4
Ari <oddwolfari@gmail.com>
2018-02-05 04:55:36 +1100

    /v fix

--- packages/LUNAR/commands/player.js
+++ packages/LUNAR/commands/player.js
@@ -18,7 +18,6 @@ mp.events.addCommand('pos', (player) =>
 mp.events.addCommand('v', (player,  _, veh_name) =>
 {
     // push to an array, so it can be deleted
-    if(player.admin < 1) return player.outputChatBox("<SERVER> You don't have access to this command!");
     if(!veh_name) return player.outputChatBox("<SERVER> The usage is: /v [model]");
 
     let pos = player.position;
