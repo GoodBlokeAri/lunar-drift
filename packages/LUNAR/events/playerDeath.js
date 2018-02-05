70f99051a9967f53fc4305a5817746736147e233
Ari <oddwolfari@gmail.com>
2018-02-06 06:06:55 +1100

    new position on hako for death spawn

--- packages/LUNAR/events/playerDeath.js
+++ packages/LUNAR/events/playerDeath.js
@@ -1,4 +1,4 @@
 mp.events.add("playerDeath", (player, reason, killer) =>
 {
-    player.spawn(new mp.Vector3(-1809.073, 823.45, 140));
+    player.spawn(new mp.Vector3(-2206.32, -448.172, 329.38));
 });
